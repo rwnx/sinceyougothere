f993fd044762309bfdf268012db8f144715e573c
Jerome Twell <jtwell1@gmail.com>
2020-10-17 10:04:40 +0100

    do not update off-screen elements

--- src/Ticker.js
+++ src/Ticker.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Ticker as NumberTicker} from "react-flip-ticker";
+import VizSensor from 'react-visibility-sensor';
 
 function prettify(x) {
     var parts = x.toString().split(".");
@@ -20,7 +21,13 @@ const UPDATE_TIMEOUT_STAGGER = 500;
 export function Ticker({timer, target}) {
     const stagger = Math.random()*UPDATE_TIMEOUT_STAGGER;
     const [dateKey, setDateKey] = useState(0);
-    window.setTimeout(() => setDateKey(dateKey + 1), UPDATE_TIMEOUT_BASE+stagger);
+    const [visible, setVisibility] = useState(true);
+    window.setTimeout(
+        () => {
+            if(visible) { setDateKey(dateKey + 1) };
+        },
+        UPDATE_TIMEOUT_BASE+stagger
+    );
     
     const rawValue = timer.unitsUntil(target);
     // ceil when values are >10, use an int (ceil) otherwise use 2sf, since it looks nicer
@@ -31,11 +38,13 @@ export function Ticker({timer, target}) {
                {timer.src ? <a className="timer__src" href={timer.src}>{timer.name}</a> : timer.name} 
             </div>
             <div className="ticker__value_container">
-                <div className="ticker__value">
-                    <NumberTicker>
-                        {value}
-                    </NumberTicker>
-                </div>
+                <VizSensor onChange={(isVisible) => { setVisibility(isVisible)}}>
+                    <div className="ticker__value">
+                        <NumberTicker>
+                            {value}
+                        </NumberTicker>
+                    </div>
+                </VizSensor>
                 <div className="ticker__unit">{timer.unit}</div>
             </div>
         </div>
